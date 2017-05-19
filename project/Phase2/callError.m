3eb63927d4d67f377f2b300ce574329cedd3434f
MuhammedKhamis <muhammedessam10@gmail.com>
2017-05-19 13:29:20 +0200

    Root finder

--- project/Phase2/callError.m
+++
@@ -1,8 +0,0 @@
-function [handles] = callError( obj, messege, handless )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    
-    handles = clearTables(handless);
-    set(obj,'String',messege); 
-end
-
